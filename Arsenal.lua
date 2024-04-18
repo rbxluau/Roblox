77334da0e4562125801da5b192a1dced0e09278e
Nahida <131898898+nahida-cn@users.noreply.github.com>
2024-04-19 04:43:31 +0800

    Create Arsenal.lua

---
+++ Arsenal.lua
@@ -0,0 +1,113 @@
+LP = game.Players.LocalPlayer
+Locale = _G.Language[LP.LocaleId] or _G.Language["en-us"]
+HRP = "HumanoidRootPart"
+Camera = workspace.CurrentCamera
+Sort = {}
+Head = {}
+
+Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/nahida-cn/Roblox/main/Library.lua"))()
+
+Window = Library:Window("SH", Locale.US)
+
+Player = Window:Tab(Locale.Player)
+
+Player:Toggle(Locale.Noclip, false, function(Value)
+    Noclip = Value
+    if not Noclip then
+        LP.Character.Humanoid:ChangeState("Jumping")
+    end
+end)
+
+Player:Toggle(Locale.Aimbot, false, function(Value)
+    Aimbot = Value
+end)
+
+Fly = Window:Tab(Locale.Fly)
+
+Fly:Slider(Locale.Speed, 0, 200, 1, function(Value)
+    Speed = Value
+end)
+
+Fly:Toggle(Locale.Toggle, false, function(Value)
+    Toggle = Value
+    for i, v in pairs(Enum.HumanoidStateType:GetEnumItems()) do
+        LP.Character.Humanoid:SetStateEnabled(v, not Toggle)
+    end
+end)
+
+Loop = Window:Tab(Locale.Loop)
+
+Loop:Dropdown(Locale.Type, "DisplayName", {"DisplayName", "Name"}, function(Value)
+    Type = Value
+end)
+
+Loop:Textbox(Locale.Name, "", true, function(Value)
+    Name = Value
+end)
+
+Loop:Toggle(Locale.TP, false, function(Value)
+    LT = Value
+end)
+
+ESP = Window:Tab(Locale.ESP)
+
+ESP:Toggle(Locale.Player, false, function(Value)
+    EP = Value
+end)
+
+About = Window:Tab(Locale.About)
+
+About:Label(Locale.By)
+
+About:Button(Locale.Copy, function()
+    setclipboard(Locale.Link)
+end)
+
+game.RunService.Stepped:Connect(function()
+    if Noclip then
+        for i, v in pairs(LP.Character:GetChildren()) do
+            if v:IsA("BasePart") then
+                v.CanCollide = false
+            end
+        end
+    end
+end)
+
+game.RunService.Heartbeat:Connect(function()
+    if Toggle then
+        LP.Character.Humanoid:ChangeState("Swimming")
+        LP.Character:TranslateBy(LP.Character.Humanoid.MoveDirection*Speed)
+        LP.Character[HRP].Velocity = Vector3.zero
+    end
+    for i, v in pairs(game.Players:GetPlayers()) do
+        if Aimbot and v.Team ~= LP.Team and v.CanLoadCharacterAppearance and #Camera:GetPartsObscuringTarget({v.Character.Head.Position}, {LP.Character, v.Character}) == 0 then
+            Distance = math.round(LP:DistanceFromCharacter(v.Character.Head.Position))
+            table.insert(Sort, Distance)
+            Head[Distance] = v.Character.Head
+        end
+        if LT and string.find(v[Type], Name) then
+            LP.Character.Humanoid.Sit = false
+            LP.Character[HRP].CFrame = v.Character[HRP].CFrame
+        end
+        if not v.Character:FindFirstChild("Highlight") then
+            Instance.new("Highlight", v.Character)
+            BG = Instance.new("BillboardGui", v.Character)
+            TL = Instance.new("TextLabel", BG)
+            BG.AlwaysOnTop = true
+            BG.Size = UDim2.new(0, 100, 0, 50)
+            BG.StudsOffset = Vector3.new(0, 4, 0)
+            TL.BackgroundTransparency = 1
+            TL.Size = UDim2.new(0, 100, 0, 50)
+        end
+        v.Character.BillboardGui.TextLabel.Text = v.Name.."\nHealth: "..math.round(v.Character.Humanoid.Health).."\nDistance: "..math.round(LP:DistanceFromCharacter(v.Character.Head.Position))
+        v.Character.BillboardGui.TextLabel.TextColor = v.TeamColor
+        v.Character.BillboardGui.Enabled = EP
+        v.Character.Highlight.Enabled = EP
+    end
+    if Aimbot and Sort[1] then
+        table.sort(Sort)
+        Camera.CFrame = CFrame.lookAt(Camera.CFrame.Position, Head[Sort[1]].Position)
+        Sort = {}
+        Head = {}
+    end
+end)
