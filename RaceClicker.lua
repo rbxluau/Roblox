db6fa77ef071e86e944045c28d2c307e5d453057
Nahida <131898898+nahida-cn@users.noreply.github.com>
2024-05-01 02:18:36 +0800

    Update RaceClicker.lua

--- RaceClicker.lua
+++ RaceClicker.lua
@@ -1,4 +1,5 @@
 LP = game.Players.LocalPlayer
+RunService = game:GetService("RunService")
 Locale = _G.Language[LP.LocaleId] or _G.Language["en-us"]
 HRP = "HumanoidRootPart"
 RS = game.ReplicatedStorage
@@ -115,7 +116,7 @@ game.UserInputService.JumpRequest:Connect(function()
     end
 end)
 
-game.RunService.Stepped:Connect(function()
+RunService.Stepped:Connect(function()
     if Noclip then
         for i, v in pairs(LP.Character:GetChildren()) do
             if v:IsA("BasePart") then
@@ -131,7 +132,7 @@ LP.leaderstats["🏁Wins"].Changed:Connect(function()
     end
 end)
 
-game.RunService.Heartbeat:Connect(function()
+RunService.Heartbeat:Connect(function()
     if Toggle then
         LP.Character.Humanoid:ChangeState("Swimming")
         LP.Character:TranslateBy(LP.Character.Humanoid.MoveDirection*Speed)
