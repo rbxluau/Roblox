7f8b6b0ff38eade8516291f642be39850c0cf504
Nahida <131898898+nahida-cn@users.noreply.github.com>
2024-05-12 18:38:29 +0800

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -1,6 +1,11 @@
-LP = game.Players.LocalPlayer
+ProximityPromptService = game:GetService("ProximityPromptService")
+UserInputService = game:GetService("UserInputService")
+VirtualUser = game:GetService("VirtualUser")
 RunService = game:GetService("RunService")
-Locale = _G.Language[LP.LocaleId] or _G.Language["en-us"]
+Lighting = game:GetService("Lighting")
+Players = game:GetService("Players")
+LocalPlayer = Players.LocalPlayer
+Locale = _G.Language[LocalPlayer.LocaleId] or _G.Language["en-us"]
 HRP = "HumanoidRootPart"
 
 Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/nahida-cn/Roblox/main/Library.lua"))()
@@ -9,12 +14,12 @@ Window = Library:Window("SH", Locale.US)
 
 Player = Window:Tab(Locale.Player)
 
-Player:Slider(Locale.WS, 0, 200, LP.Character.Humanoid.WalkSpeed, function(Value)
-    LP.Character.Humanoid.WalkSpeed = Value
+Player:Slider(Locale.WS, 0, 200, LocalPlayer.Character.Humanoid.WalkSpeed, function(Value)
+    LocalPlayer.Character.Humanoid.WalkSpeed = Value
 end)
 
-Player:Slider(Locale.JP, 0, 200, LP.Character.Humanoid.JumpPower, function(Value)
-    LP.Character.Humanoid.JumpPower = Value
+Player:Slider(Locale.JP, 0, 200, LocalPlayer.Character.Humanoid.JumpPower, function(Value)
+    LocalPlayer.Character.Humanoid.JumpPower = Value
 end)
 
 Player:Slider(Locale.Gravity, 0, 200, workspace.Gravity, function(Value)
@@ -28,7 +33,7 @@ end)
 Player:Toggle(Locale.Noclip, false, function(Value)
     Noclip = Value
     if not Noclip then
-        LP.Character.Humanoid:ChangeState("Jumping")
+        LocalPlayer.Character.Humanoid:ChangeState("Flying")
     end
 end)
 
@@ -47,7 +52,7 @@ end)
 Fly:Toggle(Locale.Toggle, false, function(Value)
     Toggle = Value
     for i, v in pairs(Enum.HumanoidStateType:GetEnumItems()) do
-        LP.Character.Humanoid:SetStateEnabled(v, not Toggle)
+        LocalPlayer.Character.Humanoid:SetStateEnabled(v, not Toggle)
     end
 end)
 
@@ -75,29 +80,29 @@ Other = Window:Tab(Locale.Other)
 
 Other:Button(Locale.BT, function()
     for i = 3, 4 do
-        HB = Instance.new("HopperBin", LP.Backpack)
+        HB = Instance.new("HopperBin", LocalPlayer.Backpack)
         HB.BinType = i
     end
 end)
 
 Other:Button(Locale.CT, function()
-    Tool = Instance.new("Tool", LP.Backpack)
+    Tool = Instance.new("Tool", LocalPlayer.Backpack)
     Tool.RequiresHandle = false
     Tool.Activated:Connect(function()
-        LP.Character[HRP].CFrame = LP:GetMouse().Hit+Vector3.new(0, 2.5, 0)
+        LocalPlayer.Character[HRP].CFrame = LocalPlayer:GetMouse().Hit+Vector3.new(0, 2.5, 0)
     end)
 end)
 
-Other:Dropdown(Locale.Camera, LP.CameraMode.Name, {"Classic", "LockFirstPerson"}, function(Value)
-    LP.CameraMode = Value
+Other:Dropdown(Locale.Camera, LocalPlayer.CameraMode.Name, {"Classic", "LockFirstPerson"}, function(Value)
+    LocalPlayer.CameraMode = Value
 end)
 
 Other:Toggle(Locale.FB, false, function(Value)
     Light = Value
     if Light then
-        game.Lighting.Ambient = Color3.new(1, 1, 1)
+        Lighting.Ambient = Color3.new(1, 1, 1)
     else
-        game.Lighting.Ambient = Color3.new(0, 0, 0)
+        Lighting.Ambient = Color3.new(0, 0, 0)
     end
 end)
 
@@ -113,15 +118,15 @@ About:Button(Locale.Copy, function()
     setclipboard(Locale.Link)
 end)
 
-game.UserInputService.JumpRequest:Connect(function()
+UserInputService.JumpRequest:Connect(function()
     if Jump then
-        LP.Character.Humanoid:ChangeState("Jumping")
+        LocalPlayer.Character.Humanoid:ChangeState("Jumping")
     end
 end)
 
 RunService.Stepped:Connect(function()
     if Noclip then
-        for i, v in pairs(LP.Character:GetChildren()) do
+        for i, v in pairs(LocalPlayer.Character:GetChildren()) do
             if v:IsA("BasePart") then
                 v.CanCollide = false
             end
@@ -129,7 +134,7 @@ RunService.Stepped:Connect(function()
     end
 end)
 
-game.ProximityPromptService.PromptButtonHoldBegan:Connect(function(v)
+ProximityPromptService.PromptButtonHoldBegan:Connect(function(v)
     if Fast then
         v.HoldDuration = 0
     end
@@ -137,14 +142,14 @@ end)
 
 RunService.Heartbeat:Connect(function()
     if Toggle then
-        LP.Character.Humanoid:ChangeState("Swimming")
-        LP.Character:TranslateBy(LP.Character.Humanoid.MoveDirection*Speed)
-        LP.Character[HRP].Velocity = Vector3.zero
+        LocalPlayer.Character.Humanoid:ChangeState("Swimming")
+        LocalPlayer.Character:TranslateBy(LocalPlayer.Character.Humanoid.MoveDirection*Speed)
+        LocalPlayer.Character[HRP].Velocity = Vector3.zero
     end
-    for i, v in pairs(game.Players:GetPlayers()) do
+    for i, v in pairs(Players:GetPlayers()) do
         if LT and string.find(v[Type], Name) then
-            LP.Character.Humanoid.Sit = false
-            LP.Character[HRP].CFrame = v.Character[HRP].CFrame
+            LocalPlayer.Character.Humanoid.Sit = false
+            LocalPlayer.Character[HRP].CFrame = v.Character[HRP].CFrame
         end
         if not v.Character:FindFirstChild("Highlight") then
             Instance.new("Highlight", v.Character)
@@ -156,21 +161,21 @@ RunService.Heartbeat:Connect(function()
             TL.BackgroundTransparency = 1
             TL.Size = UDim2.new(0, 100, 0, 50)
         end
-        v.Character.BillboardGui.TextLabel.Text = v.Name.."\nHealth: "..math.round(v.Character.Humanoid.Health).."\nDistance: "..math.round(LP:DistanceFromCharacter(v.Character.Head.Position))
+        v.Character.BillboardGui.TextLabel.Text = v.Name.."\nHealth: "..math.round(v.Character.Humanoid.Health).."\nDistance: "..math.round(LocalPlayer:DistanceFromCharacter(v.Character.Head.Position))
         v.Character.BillboardGui.TextLabel.TextColor = v.TeamColor
         v.Character.BillboardGui.Enabled = EP
         v.Character.Highlight.Enabled = EP
     end
 end)
 
-game.Lighting.LightingChanged:Connect(function()
+Lighting.LightingChanged:Connect(function()
     if Light then
-        game.Lighting.Ambient = Color3.new(1, 1, 1)
+        Lighting.Ambient = Color3.new(1, 1, 1)
     end
 end)
 
-LP.Idled:Connect(function()
+LocalPlayer.Idled:Connect(function()
     if AFK then
-        game.VirtualUser:MoveMouse(Vector2.new())
+        VirtualUser:MoveMouse(Vector2.new())
     end
 end)
