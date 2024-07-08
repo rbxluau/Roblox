c99ac616ce5a4d037e900192f50d8b37e156eea9
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-07-09 01:52:32 +0800

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -7,84 +7,68 @@ Players = game:GetService("Players")
 LocalPlayer = Players.LocalPlayer
 HRP = "HumanoidRootPart"
 
+function GetPlayers()
+    local Players = Players:GetPlayers()
+    for i, v in pairs(Players) do
+        Players[i] = v.Name
+    end
+    return Players
+end
+
 Library, Locale = loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/Library.lua"))()
 
-Window = Library:Window("SH", Locale.US)
+Window = Library:Window(Locale.US)
 
-Player = Window:Tab(Locale.Player)
+Section = Window:Tab(Locale.Player):Section("Main", true)
 
-Player:Slider(Locale.WS, 0, 200, LocalPlayer.Character.Humanoid.WalkSpeed, function(Value)
-    LocalPlayer.Character.Humanoid.WalkSpeed = Value
-end)
-
-Player:Slider(Locale.JP, 0, 200, LocalPlayer.Character.Humanoid.JumpPower, function(Value)
+Section:Slider(Locale.JP, "JumpPower", LocalPlayer.Character.Humanoid.JumpPower, 0, 200, false, function(Value)
     LocalPlayer.Character.Humanoid.JumpPower = Value
 end)
 
-Player:Slider(Locale.Gravity, 0, 200, workspace.Gravity, function(Value)
+Section:Slider(Locale.Gravity, "Gravity", workspace.Gravity, 0, 200, false, function(Value)
     workspace.Gravity = Value
 end)
 
-Player:Toggle(Locale.IJ, false, function(Value)
-    Jump = Value
+Section:Slider(Locale.Boost, "Boost", 0, 0, 200)
+
+Section:Toggle(Locale.Fly, "Fly", false, function(Value)
+    for i, v in pairs(Enum.HumanoidStateType:GetEnumItems()) do
+        LocalPlayer.Character.Humanoid:SetStateEnabled(v, not Value)
+    end
 end)
 
-Player:Toggle(Locale.Noclip, false, function(Value)
-    Noclip = Value
-    if not Noclip then
+Section:Toggle(Locale.IJ, "InfJump")
+
+Section:Toggle(Locale.Noclip, "Noclip", false, function(Value)
+    if not Value then
         LocalPlayer.Character.Humanoid:ChangeState("Flying")
     end
 end)
 
-Interact = Window:Tab(Locale.Interact)
+Section = Window:Tab(Locale.Interact):Section("Main", true)
 
-Interact:Toggle(Locale.Fast, false, function(Value)
-    Fast = Value
-end)
+Section:Toggle(Locale.Fast, "Fast")
 
-Fly = Window:Tab(Locale.Fly)
+Section = Window:Tab(Locale.Loop):Section("Main", true)
 
-Fly:Slider(Locale.Speed, 0, 200, 1, function(Value)
-    Speed = Value
-end)
+Player = Section:Dropdown(Locale.Player, "Player", GetPlayers())
 
-Fly:Toggle(Locale.Toggle, false, function(Value)
-    Toggle = Value
-    for i, v in pairs(Enum.HumanoidStateType:GetEnumItems()) do
-        LocalPlayer.Character.Humanoid:SetStateEnabled(v, not Toggle)
-    end
-end)
+Section:Toggle(Locale.TP, "Teleport")
 
-Loop = Window:Tab(Locale.Loop)
+Section = Window:Tab(Locale.ESP):Section("Main", true)
 
-Loop:Dropdown(Locale.Type, "DisplayName", {"DisplayName", "Name"}, function(Value)
-    Type = Value
-end)
+Section:Toggle(Locale.Player, "ESP")
 
-Loop:Textbox(Locale.Name, "", true, function(Value)
-    Name = Value
-end)
+Section = Window:Tab(Locale.Other):Section("Main", true)
 
-Loop:Toggle(Locale.TP, false, function(Value)
-    LT = Value
-end)
-
-ESP = Window:Tab(Locale.ESP)
-
-ESP:Toggle(Locale.Player, false, function(Value)
-    EP = Value
-end)
-
-Other = Window:Tab(Locale.Other)
-
-Other:Button(Locale.BT, function()
+Section:Button(Locale.BT, function()
     for i = 3, 4 do
         HB = Instance.new("HopperBin", LocalPlayer.Backpack)
         HB.BinType = i
     end
 end)
 
-Other:Button(Locale.CT, function()
+Section:Button(Locale.CT, function()
     Tool = Instance.new("Tool", LocalPlayer.Backpack)
     Tool.RequiresHandle = false
     Tool.Activated:Connect(function()
@@ -92,39 +76,36 @@ Other:Button(Locale.CT, function()
     end)
 end)
 
-Other:Dropdown(Locale.Camera, LocalPlayer.CameraMode.Name, {"Classic", "LockFirstPerson"}, function(Value)
+Section:Dropdown(Locale.Camera, "Camera", {"Classic", "LockFirstPerson"}, function(Value)
     LocalPlayer.CameraMode = Value
 end)
 
-Other:Toggle(Locale.FB, false, function(Value)
-    Light = Value
-    if Light then
+Section:Toggle(Locale.FB, "Light", false, function(Value)
+    if Value then
         Lighting.Ambient = Color3.new(1, 1, 1)
     else
         Lighting.Ambient = Color3.new(0, 0, 0)
     end
 end)
 
-Other:Toggle(Locale.AFK, false, function(Value)
-    AFK = Value
-end)
+Section:Toggle(Locale.AFK, "AFK")
 
-About = Window:Tab(Locale.About)
+Section = Window:Tab(Locale.About):Section("Main", true)
 
-About:Label(Locale.By)
+Section:Label(Locale.By)
 
-About:Button(Locale.Copy, function()
+Section:Button(Locale.Copy, function()
     setclipboard(Locale.Link)
 end)
 
 UserInputService.JumpRequest:Connect(function()
-    if Jump then
+    if Library.flags.InfJump then
         LocalPlayer.Character.Humanoid:ChangeState("Jumping")
     end
 end)
 
 RunService.Stepped:Connect(function()
-    if Noclip then
+    if Library.flags.Noclip then
         for i, v in pairs(LocalPlayer.Character:GetChildren()) do
             if v:IsA("BasePart") then
                 v.CanCollide = false
@@ -134,22 +115,30 @@ RunService.Stepped:Connect(function()
 end)
 
 ProximityPromptService.PromptButtonHoldBegan:Connect(function(v)
-    if Fast then
+    if Library.flags.Fast then
         v.HoldDuration = 0
     end
 end)
 
+Players.PlayerAdded:Connect(function(v)
+    Player:AddOption(v.Name)
+end)
+
+Players.PlayerRemoving:Connect(function(v)
+    Player:RemoveOption(v.Name)
+end)
+
 RunService.Heartbeat:Connect(function()
-    if Toggle then
+    LocalPlayer.Character:TranslateBy(LocalPlayer.Character.Humanoid.MoveDirection*Library.flags.Boost)
+    if Library.flags.Fly then
         LocalPlayer.Character.Humanoid:ChangeState("Swimming")
-        LocalPlayer.Character:TranslateBy(LocalPlayer.Character.Humanoid.MoveDirection*Speed)
         LocalPlayer.Character[HRP].Velocity = Vector3.zero
     end
+    if Teleport then
+        LocalPlayer.Character.Humanoid.Sit = false
+        LocalPlayer.Character[HRP].CFrame = Players[Library.flags.Player].Character[HRP].CFrame
+    end
     for i, v in pairs(Players:GetPlayers()) do
-        if LT and string.find(v[Type], Name) then
-            LocalPlayer.Character.Humanoid.Sit = false
-            LocalPlayer.Character[HRP].CFrame = v.Character[HRP].CFrame
-        end
         if not v.Character:FindFirstChild("Highlight") then
             Instance.new("Highlight", v.Character)
             BG = Instance.new("BillboardGui", v.Character)
@@ -162,19 +151,20 @@ RunService.Heartbeat:Connect(function()
         end
         v.Character.BillboardGui.TextLabel.Text = v.Name.."\nHealth: "..math.round(v.Character.Humanoid.Health).."\nDistance: "..math.round(LocalPlayer:DistanceFromCharacter(v.Character.Head.Position))
         v.Character.BillboardGui.TextLabel.TextColor = v.TeamColor
-        v.Character.BillboardGui.Enabled = EP
-        v.Character.Highlight.Enabled = EP
+        v.Character.Highlight.FillColor = v.TeamColor.Color
+        v.Character.BillboardGui.Enabled = Library.flags.ESP
+        v.Character.Highlight.Enabled = Library.flags.ESP
     end
 end)
 
 Lighting.LightingChanged:Connect(function()
-    if Light then
+    if Library.flags.Light then
         Lighting.Ambient = Color3.new(1, 1, 1)
     end
 end)
 
 LocalPlayer.Idled:Connect(function()
-    if AFK then
+    if Library.flags.AFK then
         VirtualUser:MoveMouse(Vector2.new())
     end
 end)
