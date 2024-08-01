1324f03378437813a461c7af01701ebf36cc40a4
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-08-02 00:23:51 +0800

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,8 +1,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/"..(({
+    [2820580801] = "Ohio",
     [111958650] = "Arsenal",
     [2440500124] = "Doors",
     [3476371299] = "RaceClicker",
-    [4777817887] = "BladeBall",
     [3085257211] = "RainbowFriends",
     [73885730] = "PrisonLife"
 })[game.GameId] or "Universal")..".lua"))()
