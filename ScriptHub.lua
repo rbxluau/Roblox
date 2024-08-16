c5c4a3827b60f0da923108676f96327bf1d24ef1
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-08-16 16:30:00 +0800

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,8 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/"..(({
-    [2820580801] = "Ohio",
-    [111958650] = "Arsenal",
-    [2440500124] = "Doors",
-    [3476371299] = "RaceClicker",
-    [3085257211] = "RainbowFriends",
-    [73885730] = "PrisonLife"
-})[game.GameId] or "Universal")..".lua"))()
+Instance.new("Message",workspace).Text="最近听说习近平在北戴河突发疾病"
