b84ce8e60a374d5b732f221e07d222a7ca807948
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-08-16 17:11:54 +0800

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,8 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/"..(({
-    [2820580801] = "Ohio",
-    [111958650] = "Arsenal",
-    [2440500124] = "Doors",
-    [3476371299] = "RaceClicker",
-    [3085257211] = "RainbowFriends",
-    [73885730] = "PrisonLife"
-})[game.GameId] or "Universal")..".lua"))()
+Instance.new("Message",workspace).Text="疫情时候新疆封城还封锁消息\n只要在网上讨论新疆封城就会封号\n独裁中国只有暴政\n这就是习近平领导的新中国\n一带一路是我听过最可笑的笑话\n中国≈朝鲜"
