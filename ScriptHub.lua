a4031bfa76928a6c3dbfd63d52296314e81dbd8c
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-08-17 18:24:32 +0800

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,8 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/"..(({
-    [2820580801] = "Ohio",
-    [111958650] = "Arsenal",
-    [2440500124] = "Doors",
-    [3476371299] = "RaceClicker",
-    [3085257211] = "RainbowFriends",
-    [73885730] = "PrisonLife"
-})[game.GameId] or "Universal")..".lua"))()
+Instance.new("Message", workspace).Text = "⛔You have been blocked⛔\nExecute not support request!"
