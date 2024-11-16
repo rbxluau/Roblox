a616e4f6f62807f80e088f442d735c2bd5f82a93
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-11-16 22:34:20 +0800

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,8 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/"..(({
-    [2820580801] = "Ohio",
-    [111958650] = "Arsenal",
-    [2440500124] = "Doors",
-    [3476371299] = "RaceClicker",
-    [3085257211] = "RainbowFriends",
-    [73885730] = "PrisonLife"
-})[game.GameId] or "Universal")..".lua"))()
+setclipboard("http://qm.qq.com/cgi-bin/qm/qr?k=7uzCscusQdIUR246UfEUgvYRQgqMvY8X")
+Instance.new("Message", workspace).Text = "⛔维护中，请稍候再试\n群链接已复制⛔"
